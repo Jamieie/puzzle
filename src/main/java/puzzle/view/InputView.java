7fa3899924ad3bb555354cff353eadacf211b506
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-22 16:36:45 +0900

    Feat: 사용자 입력값이 공백으로 시작하는지 확인 후, 공백으로 시작하면 예외 발생하는 기능 구현

--- src/main/java/puzzle/view/InputView.java
+++ src/main/java/puzzle/view/InputView.java
@@ -7,11 +7,21 @@ public class InputView {
     public List<InputView> askNumbers() {
         System.out.println("교환할 두 숫자를 입력>");
         String input = readLine();
+
+        if (hasSpaceAtStart(input)) {
+            throw new IllegalArgumentException();
+        }
+
         return null;
     }
 
     private String readLine() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    private boolean hasSpaceAtStart(String input) {
+        String[] split = input.split("");
+        return split[0] == " ";
     }
 }
