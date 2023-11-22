0a3a711c61cf0bfbba56680a67340291c19a38e1
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-22 17:21:53 +0900

    Feat: 입력값 숫자 리스트로 변환하는 기능 구현

--- src/main/java/puzzle/view/InputView.java
+++ src/main/java/puzzle/view/InputView.java
@@ -1,10 +1,13 @@
 package puzzle.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-    public List<InputView> askNumbers() {
+    public List<Integer> askNumbers() {
         System.out.println("교환할 두 숫자를 입력>");
         String input = readLine();
 
@@ -12,12 +15,24 @@ public class InputView {
             throw new IllegalArgumentException();
         }
 
-        return null;
+        List<Integer> inputNumbers = new ArrayList<>();
+        try {
+            inputNumbers = toIntegers(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return inputNumbers;
     }
 
     private String readLine() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    private List<Integer> toIntegers(String input) {
+        return Arrays.stream(input.trim().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     private boolean hasSpaceAtStart(String input) {
