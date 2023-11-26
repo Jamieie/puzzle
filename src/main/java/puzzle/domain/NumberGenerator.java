e0ecc96b5f8f38b64cd0edf61c1cd4e37c56e8e7
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 11:24:57 +0900

    Feat: 퍼즐 숫자 배열 생성하는 기능 구현

--- src/main/java/puzzle/domain/NumberGenerator.java
+++ src/main/java/puzzle/domain/NumberGenerator.java
@@ -1,9 +1,39 @@
 package puzzle.domain;
 
-import java.util.List;
+import java.util.*;
 
 public class NumberGenerator {
-    public List<Row> createRows() {
-        return null;
+    private final int PUZZLE_SIZE = 16;
+    private final int LIST_SIZE = 4;
+    private final int MAP_SIZE = 4;
+    private List<Integer> allNumbers;
+
+    public NumberGenerator() {
+        createNumbers();
+    }
+
+    private void createNumbers() {
+        allNumbers = new ArrayList<>();
+        for (int i = 0; i < PUZZLE_SIZE; i++) {
+            allNumbers.add(i+1);
+        }
+        Collections.shuffle(allNumbers);
+    }
+
+    public Map<Integer, List<Integer>> createLists() {
+        Map<Integer, List<Integer>> lists = new HashMap<>();
+        for (int i = 0; i < MAP_SIZE; i++) {
+            lists.put(i, createList());
+        }
+        return lists;
+    }
+
+    private List<Integer> createList() {
+        List<Integer> rowNumbers = new ArrayList<>();
+        for (int i = 0; i < LIST_SIZE; i++) {
+            rowNumbers.add(allNumbers.get(0));
+            allNumbers.remove(0);
+        }
+        return rowNumbers;
     }
 }
---
+++ src/test/java/puzzle/domain/NumberGeneratorTest.java
@@ -0,0 +1,26 @@
+package puzzle.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+class NumberGeneratorTest {
+
+    NumberGenerator generator;
+
+    @BeforeEach
+    void setUp() {
+        generator = new NumberGenerator();
+    }
+
+    @DisplayName("네 개의 숫자 리스트를 가진 map 생성")
+    @Test
+    void createRows() {
+        Map<Integer, List<Integer>> lists = generator.createLists();
+        Assertions.assertThat(lists.size()).isEqualTo(4);
+    }
+}
