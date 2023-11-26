64abd498032575b319f1ada89c012e2cf029bd4d
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 16:48:04 +0900

    Feat: 사용자로부터 숫자 입력 받는 기능 구현

--- src/main/java/puzzle/view/InputView.java
+++ src/main/java/puzzle/view/InputView.java
@@ -1,4 +1,18 @@
 package puzzle.view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputView {
+    public int readLine() {
+        System.out.print("숫자 입력> ");
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        try {
+            input = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다. 다시 입력해 주세요.");
+        }
+        return input;
+    }
 }
