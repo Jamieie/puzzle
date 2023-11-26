42e6db62e0af1f7361f9bb9573d2bbdf879862b4
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 16:31:47 +0900

    Feat: 퍼즐이 오름차순으로 정렬되어 있는지 확인하는 기능 구현

--- src/main/java/puzzle/domain/Puzzle.java
+++ src/main/java/puzzle/domain/Puzzle.java
@@ -2,9 +2,13 @@ package puzzle.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class Puzzle {
     private final int VACANCY_NUMBER = 16;
+    private final int FIRST_INDEX = 0;
+    private final int LAST_INDEX = 0;
+
     private Map<Integer, List<Integer>> numbers;
 
     public Puzzle(Map<Integer, List<Integer>> numbers) {
@@ -50,5 +54,31 @@ public class Puzzle {
             }
         }
         return false;
+    }
+
+    public boolean isSorted() {
+        // 각 열이 오름차순으로 정렬되어 있는지 확인
+        for (int i = 0; i < numbers.size(); i++) {
+            List<Integer> row = numbers.get(i);
+            if (!isSortedRow(row)) {
+                return false;
+            }
+        }
+        // 각 열의 마지막 숫자 보다 뒷열의 첫번째 숫자가 큰지 확인
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            List<Integer> row = numbers.get(i);
+            List<Integer> rowAfter = numbers.get(i + 1);
+            if (!(row.get(LAST_INDEX) < rowAfter.get(FIRST_INDEX))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSortedRow(List<Integer> row) {
+        if (row == null || row.size() <= 1) {
+            return true;
+        }
+        return IntStream.range(0, row.size()-1).allMatch(i -> row.get(i) < row.get(i+1));
     }
 }
--- src/test/java/puzzle/domain/PuzzleTest.java
+++ src/test/java/puzzle/domain/PuzzleTest.java
@@ -1,6 +1,7 @@
 package puzzle.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,29 +15,67 @@ import static org.assertj.core.api.Assertions.*;
 class PuzzleTest {
     private Puzzle puzzle;
 
-    private final Map<Integer, List<Integer>> numbers = Map.of(
+    private final Map<Integer, List<Integer>> NUMBERS_SORTED = Map.of(
+            0, new ArrayList<>(Arrays.asList(1,2,3,4)),
+            1, new ArrayList<>(Arrays.asList(5,6,7,8)),
+            2, new ArrayList<>(Arrays.asList(9,10,11,12)),
+            3, new ArrayList<>(Arrays.asList(13,14,15,16))
+    );
+
+    private final Map<Integer, List<Integer>> NUMBERS_UNSORTED_1 = Map.of(
             0, new ArrayList<>(Arrays.asList(1,2,3,4)),
             1, new ArrayList<>(Arrays.asList(5,6,7,8)),
             2, new ArrayList<>(Arrays.asList(9,10,16,12)),
             3, new ArrayList<>(Arrays.asList(13,14,15,11))
     );
 
+    private final Map<Integer, List<Integer>> NUMBERS_UNSORTED_2 = Map.of(
+            0, new ArrayList<>(Arrays.asList(1,2,3,4)),
+            1, new ArrayList<>(Arrays.asList(5,6,7,8)),
+            2, new ArrayList<>(Arrays.asList(13,14,15,16)),
+            3, new ArrayList<>(Arrays.asList(9,10,11,12))
+    );
+
     @DisplayName("특정 값을 빈자리로 옮기는 기능")
     @CsvSource({"7,1", "10,2", "12,2", "15,3"})
     @ParameterizedTest
     void move(int input, int row) {
-        puzzle = new Puzzle(numbers);
+        puzzle = new Puzzle(NUMBERS_UNSORTED_1);
         puzzle.move(input);
-        assertThat(numbers.get(2)).contains(input);
-        assertThat(numbers.get(row)).contains(16);
+        assertThat(NUMBERS_UNSORTED_1.get(2)).contains(input);
+        assertThat(NUMBERS_UNSORTED_1.get(row)).contains(16);
     }
 
     @DisplayName("특정값 주변에 빈자리가 있는지 확인하는 기능")
     @CsvSource({"7,true", "10,true", "12,true", "15,true", "6,false", "8,false", "14,false", "11,false", "3,false"})
     @ParameterizedTest
     void hasVacancyNear(int value, boolean expected) {
-        puzzle = new Puzzle(numbers);
+        puzzle = new Puzzle(NUMBERS_UNSORTED_1);
         boolean result = puzzle.hasVacancyNear(value);
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("퍼즐이 오름차순으로 정렬되어 있으면 true를 반환한다.")
+    @Test
+    void isSorted_sorted() {
+        puzzle = new Puzzle(NUMBERS_SORTED);
+        boolean result = puzzle.isSorted();
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("퍼즐이 오름차순으로 정렬되어 있지 않으면 false를 반환한다.")
+    @Test
+    void isSorted_unsorted1() {
+        puzzle = new Puzzle(NUMBERS_UNSORTED_1);
+        boolean result = puzzle.isSorted();
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("퍼즐이 오름차순으로 정렬되어 있지 않으면 false를 반환한다.")
+    @Test
+    void isSorted_unsorted2() {
+        puzzle = new Puzzle(NUMBERS_UNSORTED_2);
+        boolean result = puzzle.isSorted();
+        assertThat(result).isFalse();
     }
 }
