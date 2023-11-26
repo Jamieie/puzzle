85490712e43163eafa9c1ae78ffab4417f783848
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 11:34:04 +0900

    Feat: 특정 값의 퍼즐 내에서의 위치 좌표 구하는 기능 구현

--- src/main/java/puzzle/domain/Puzzle.java
+++ src/main/java/puzzle/domain/Puzzle.java
@@ -1,11 +1,30 @@
 package puzzle.domain;
 
 import java.util.List;
+import java.util.Map;
 
 public class Puzzle {
-    private List<Row> rows;
+    private final int VACANCY_NUMBER = 16;
+    private Map<Integer, List<Integer>> numbers;
 
-    public Puzzle(List<Row> rows) {
-        this.rows = rows;
+    public Puzzle(Map<Integer, List<Integer>> numbers) {
+        this.numbers = numbers;
+    }
+
+    private Coordinate findPosition(int value) {
+        Coordinate coordinate = new Coordinate();
+        for (int i = 0; i < numbers.size(); i++) {
+            List<Integer> row = numbers.get(i);
+            int index = row.indexOf(value);
+            if (index != -1) {
+                coordinate.setY(i);
+                coordinate.setX(index);
+            }
+        }
+        return coordinate;
+    }
+
+    private Coordinate findVacancy() {
+        return findPosition(VACANCY_NUMBER);
     }
 }
