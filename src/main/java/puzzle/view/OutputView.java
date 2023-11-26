2ae74bedde4757aaa7082b8a97849226560dd718
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 17:25:45 +0900

    Feat: 퍼즐 배열 및 메시지 출력하는 기능 구현

--- src/main/java/puzzle/view/OutputView.java
+++ src/main/java/puzzle/view/OutputView.java
@@ -1,4 +1,42 @@
 package puzzle.view;
 
+import puzzle.domain.Puzzle;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
+    public void printTitle() {
+        System.out.println("재미있는 15 퍼즐!");
+    }
+
+    public void printTurn(int turn) {
+        System.out.println("Turn " + turn);
+    }
+
+    public void printPuzzle(Puzzle puzzle) {
+        Map<Integer, List<Integer>> numbers = puzzle.getNumbers();
+        for (int i = 0; i < numbers.size(); i++) {
+            printRow(numbers.get(i));
+        }
+    }
+
+    private void printRow(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 10){
+                System.out.print("[ " + number + "]");
+            }
+            if (number >= 10 && number < 16) {
+                System.out.print("[" + number + "]");
+            }
+            if (number == 16) {
+                System.out.print("[  ]");
+            }
+        }
+        System.out.println();
+    }
+
+    public void printSuccess(int turn) {
+        System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성했습니다!");
+    }
 }
