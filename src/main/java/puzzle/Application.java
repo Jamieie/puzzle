af01c3da63842fc8e90860357e226cc82fafb739
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-26 17:44:10 +0900

    Feat: 사용자로부터 값 입력받는 기능 구현

--- src/main/java/puzzle/Application.java
+++ src/main/java/puzzle/Application.java
@@ -1,7 +1,37 @@
 package puzzle;
 
+import puzzle.domain.Puzzle;
+import puzzle.util.Validation;
+import puzzle.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    private static int askNumber(Puzzle puzzle) {
+        InputView inputView = new InputView();
+        int input;
+        while (true) {
+            try {
+                input = inputView.readLine();
+                validate(puzzle, input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println();
+                System.out.println(e.getMessage());
+                System.out.println();
+            }
+        }
+        return input;
+    }
+
+    private static void validate(Puzzle puzzle, int input) {
+        Validation validation = new Validation();
+        boolean inRange = validation.isInRange(input);
+        boolean vacancyNear = puzzle.hasVacancyNear(input);
+        if (!inRange && !vacancyNear) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다. 다시 입력해 주세요.");
+        }
     }
 }
